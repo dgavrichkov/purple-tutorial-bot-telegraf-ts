4130bbf9cf7e8e8e833211c05a48c81717e70e59
ditmar <dgavrichkov32@gmail.com>
2023-01-22 19:42:02 +0300

    сервис конфигурации и dotenv

---
+++ src/config/config.service.ts
@@ -0,0 +1,26 @@
+// Сервис конфигурации
+import { config, DotenvParseOutput } from 'dotenv';
+import { IConfigService } from './config.interface';
+
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+
+	constructor() {
+		const { error, parsed } = config();
+		if(error) {
+			throw new Error('не найдет файл .env');
+		}
+		if(!parsed) {
+			throw new Error('Пустой .env');
+		}
+		this.config = parsed;
+	}
+
+	get(key: string): string {	
+		const res = this.config[key];
+		if(!res) {
+			throw new Error('Нет такого ключа')
+		}
+		return res;
+	}
+}
